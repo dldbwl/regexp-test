fa753f26714335c25f91132243762098136e0cbd
dldbwl <euz07040704@gmail.com>
2022-01-17 04:02:22 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,11 @@
+const str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+http://localhost:1234
+동해물과 백두산이 마르고 닳도록
+`;
+
+console.log(str.match(/.{1,}(?=@)/g));
